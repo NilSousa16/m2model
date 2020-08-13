7fa605f60857019aa01e1fe19caa899f7f2f396f
NilSousa16 <nil.sousa16@gmail.com>
2020-08-13 10:35:02 -0300

    Entity mapping

---
+++ src/main/java/br/ufba/dcc/wiser/m2model/model/Actuador.java
@@ -0,0 +1,5 @@
+package br.ufba.dcc.wiser.m2model.model;
+
+public class Actuador {
+
+}
---
+++ src/main/java/br/ufba/dcc/wiser/m2model/model/Gateway.java
@@ -0,0 +1,33 @@
+package br.ufba.dcc.wiser.m2model.model;
+
+public class Gateway {
+
+	private String mac;
+	private String ip;
+	private String hostName;
+
+	public String getMac() {
+		return mac;
+	}
+
+	public void setMac(String mac) {
+		this.mac = mac;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public String getHostName() {
+		return hostName;
+	}
+
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+
+}
