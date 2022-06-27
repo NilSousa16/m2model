f0fa5e8252eb2d606ad54d9306b7102731d7de98
NilSousa16 <nil.sousa16@gmail.com>
2022-06-27 11:03:14 -0300

    Model update

---
+++ src/main/java/br/ufba/dcc/wiser/m2model/model/Device.java
@@ -0,0 +1,140 @@
+package br.ufba.dcc.wiser.m2model.model;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Device implements Serializable {
+
+	private static final long serialVersionUID = -2042913458886286579L;
+
+	@Id
+	private String id;
+	/**
+	 * String with latitude and longitude
+	 */
+	private String location;
+	private String description;
+	/**
+	 * Indicates whether the sensor is for temperature, humidity, etc.
+	 */
+	private String typeSensor;
+	/**
+	 * Indicates whether the device is working 
+	 */
+	private Boolean status;
+	/**
+	 * Device registration date
+	 */
+	@Temporal(TemporalType.TIMESTAMP)
+	private Calendar date;
+
+	/**
+	 * Gateway connected to device
+	 */
+	@ManyToOne
+	private Gateway gateway;
+
+	public Device() {
+	}
+
+	public Device(String location, String description, String typeSensor, Boolean status, Calendar date,
+			Gateway gateway) {
+		super();
+		this.location = location;
+		this.description = description;
+		this.typeSensor = typeSensor;
+		this.status = status;
+		this.date = date;
+		this.gateway = gateway;
+	}
+	
+	public Device(String id) {
+		super();
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getTypeSensor() {
+		return typeSensor;
+	}
+
+	public void setTypeSensor(String typeSensor) {
+		this.typeSensor = typeSensor;
+	}
+
+	public Boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+
+	public Calendar getDate() {
+		return date;
+	}
+
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
+
+	public Gateway getGateway() {
+		return gateway;
+	}
+
+	public void setGateway(Gateway gateway) {
+		this.gateway = gateway;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(date, description, gateway, id, location, status, typeSensor);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Device other = (Device) obj;
+		return Objects.equals(date, other.date) && Objects.equals(description, other.description)
+				&& Objects.equals(gateway, other.gateway) && Objects.equals(id, other.id)
+				&& Objects.equals(location, other.location) && Objects.equals(status, other.status)
+				&& Objects.equals(typeSensor, other.typeSensor);
+	}
+
+}
---
+++ src/main/java/br/ufba/dcc/wiser/m2model/model/DeviceStatus.java
@@ -0,0 +1,68 @@
+package br.ufba.dcc.wiser.m2model.model;
+
+import java.util.Calendar;
+import java.util.Objects;
+
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+public class DeviceStatus {
+	
+	@Id
+	@ManyToOne
+	private Device device;
+
+	@Id
+	@Temporal(TemporalType.TIMESTAMP)
+	private Calendar date;
+	
+	/**
+	 * Currently operating: operational / testing / unused
+	 */
+	private String situation;
+
+	public Calendar getDate() {
+		return date;
+	}
+
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
+
+	public Device getDevice() {
+		return device;
+	}
+
+	public void setDevice(Device device) {
+		this.device = device;
+	}
+
+	public String getSituation() {
+		return situation;
+	}
+
+	public void setSituation(String situation) {
+		this.situation = situation;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(date, device, situation);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DeviceStatus other = (DeviceStatus) obj;
+		return Objects.equals(date, other.date) && Objects.equals(device, other.device)
+				&& Objects.equals(situation, other.situation);
+	}
+
+}
